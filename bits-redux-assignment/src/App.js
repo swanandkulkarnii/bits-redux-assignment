f988bc20c7e63059bded0bc4ec6376a48b425dce
bits-swanandkulakrni <89059982+swanandkulkarnii@users.noreply.github.com>
2022-01-05 15:27:45 +0530

    Add files via upload

--- bits-redux-assignment/src/App.js
+++ bits-redux-assignment/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./ConnorsGroup.Client.React/features/app/users/components/Home";
 import UsersList from "./ConnorsGroup.Client.React/features/app/users/components/UsersList";
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <UsersList />
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/users" element={<UsersList />} exact></Route>
+      </Routes>
     );
   }
 }
