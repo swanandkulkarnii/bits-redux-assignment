6022348d064a729b5faa40b4041c8291f9562609
bits-swanandkulakrni <89059982+swanandkulkarnii@users.noreply.github.com>
2022-01-05 13:16:16 +0530

    Delete bits-redux-assignment directory

--- bits-redux-assignment/src/ConnorsGroup.Client.React/features/app/users/components/UserDetailView.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const UserDetailView = () => {
-  return <div></div>;
-};
-
-export default UserDetailView;
--- bits-redux-assignment/src/ConnorsGroup.Client.React/rootReducer.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import usersReducer from "./features/app/users/reducers/usersReducer";
-
-const rootReducer = combineReducers({ usersReducer });
-
-export default rootReducer;
