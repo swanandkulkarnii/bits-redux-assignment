bc6b8ce1cbc9202a146427573c3064ac35236745
bits-swanandkulakrni <89059982+swanandkulkarnii@users.noreply.github.com>
2022-01-06 12:24:35 +0530

    Add files via upload

--- bits-redux-assignment/src/ConnorsGroup.Client.React/features/app/users/reducers/usersReducer.js
+++ bits-redux-assignment/src/ConnorsGroup.Client.React/features/app/users/reducers/usersReducer.js
@@ -1,12 +1,16 @@
 import { Map } from "immutable";
 
-const initialState = Map({ users: [], total: [] });
+const initialState = Map({ users: [], total: [], isLoading: false });
 
 const usersReducer = (state = initialState, action) => {
   switch (action.type) {
+    case "USERS_DATA_IS_LOADING":
+      return { ...state, isLoading: true };
+
     case "FETCH_USERS_DATA":
       return {
         ...state,
+        isLoading: false,
         users: action.payload.data.data,
         total: action.payload.data.meta.pagination.total,
       };
